06060ae26e22fc676b086d029d594601295c4dd0
James Chapman <james.chapman.19@ucl.ac.uk>
2022-07-21 15:19:51 +0100

    Remove example

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 from setuptools import setup
 
-with open('requirements.txt', "r") as f:
-    required = f.read()
-
 setup(
     name='pysven',
     version='0',
     packages=['pysven'],
     url="https://github.com/jameschapman19/pysven",
     license='MIT',
-    requires=required,
     author='James Chapman',
     author_email='james.chapman.19@ucl.ac.uk',
     description='Elastic Net implementation using SVM solver'
