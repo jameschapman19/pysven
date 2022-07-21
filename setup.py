b3d668de7c40ab4dca6a1293446e96582df4e5f6
James Chapman <james.chapman.19@ucl.ac.uk>
2022-07-21 15:14:16 +0100

    Remove example

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-with open('requirements.txt') as f:
+with open('requirements.txt', "r") as f:
     required = f.read().splitlines()
 
 setup(
     name='pysven',
     version='0',
     packages=['pysven'],
-    url='',
-    license='',
+    url="https://github.com/jameschapman19/pysven",
+    license='MIT',
     requires=required,
     author='James Chapman',
     author_email='james.chapman.19@ucl.ac.uk',
